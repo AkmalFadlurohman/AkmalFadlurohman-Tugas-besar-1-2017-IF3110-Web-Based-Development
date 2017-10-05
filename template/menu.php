72786d6d2a136e49a3a9ad8eeb9c5159b1ab8689
ayamberkakienam <zackhardyz@gmail.com>
2017-10-05 13:24:57 +0700

    Modifikasi tampilan default

--- template/menu.php
+++ template/menu.php
@@ -2,17 +2,17 @@
     echo '
     <a href="order/order.php" name="order_link">
 	<div class="menu" id="order_link">
-		<h2>Order</h2>
+		<h3>Order</h3>
 	</div>
 	</a>
 	<a href="history/history.php" name="history_link">
 	<div class="menu" id="history_link">
-		<h2>History</h2>
+		<h3>History</h3>
 	</div>
 	</a>
 	<a href="profile/profile.php" name="profile_link">
 	<div class="menu" id="profile_link">
-		<h2>My Profile</h2>
+		<h3>My Profile</h3>
 	</div>
 	</a>
     ';
