0a9bb7ed9a0807ebcd1e1175e4febff840eebfc6
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2017-10-06 19:34:00 +0800

    Bikin page awal history. ganti menu.php buat direct ke laman yang benar. kelas subheader, title, dan title h1 dipindah ke default_style.css

---
+++ history/transaction_history.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>transaction history</title>
+	<link rel="stylesheet" type="text/css" href="../css/default_style.css">
+    <link rel="stylesheet" type="text/css" href="../css/history.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+</head>
+<body>
+	<div class="frame">
+		<div class="header">
+			<?php
+                $user_id = $_GET['id'];
+                include '../database/dbconnect.php';
+                
+                $query=mysqli_query($con,"SELECT * FROM user WHERE user_id='".$user_id."'") or die(mysqli_error());
+                
+                if(mysqli_num_rows($query)!=0)
+                {
+                    $row=mysqli_fetch_assoc($query);
+                    $username = $row['username'];
+                    include("../template/header.php");
+                }
+                mysqli_close($con);
+            ?>
+		</div>
+		<div class="menu_container">
+            <?php include'../template/menu.php';?>
+        </div>
+        <div class="history_container">
+        	<div class="subheader">
+        		<div class="title"><h1>Transaction History</h1></div>
+        	</div>
+        </div>
+	</div>
+</body>
+</html>
